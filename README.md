5c207e6dcb444e5a6f98389f9820df95f1756db8
Vladimir Starkov <matmuchrapna@gmail.com>
2015-04-09 02:20:27 +0200

    feature(dashes): add en and em dashes support

--- README.md
+++ README.md
@@ -26,37 +26,35 @@ Check *index.js* for list of used typographic modules and for text samples for p
 
 Thanks to http://semver.org/ for text sample. Btw, I modified them to reflect all used modules.
 
-### Input
-```md
+#### Input
+
 In "the 'world' of software management" there exists a dread place called
 "dependency hell."  The bigger your system grows and the more packages you
-integrate into your software,  the more likely you are to find yourself,
+integrate into your software —  the more likely you are to find yourself,
 one day, in this pit of despair...
 
 'This is not a new or "revolutionary" idea'.  In fact,  you probably do something
-close to this already.  The problem is that "close" isn't good enough.
+close to this already in 1999-2014.  The problem -- that "close" isn't good enough.
 Without compliance to some sort of formal specification,  version numbers
 are essentially useless for dependency management.  By giving a name and
 clear definition to the above ideas,  it becomes easy to communicate your
 intentions to the users of your software.  Once these intentions are clear,
 flexible (but not too flexible) dependency specifications' can finally be made.
-```
 
-### Output
-```md
+#### Output
+
 In “the ‘world’ of software management” there exists a dread place called
 “dependency hell.” The bigger your system grows and the more packages you
-integrate into your software, the more likely you are to find yourself,
+integrate into your software — the more likely you are to find yourself,
 one day, in this pit of despair…
 
 “This is not a new or ‘revolutionary’ idea”. In fact, you probably do something
-close to this already. The problem is that “close” isn’t good enough.
+close to this already in 1999–2014. The problem — that “close” isn’t good enough.
 Without compliance to some sort of formal specification, version numbers
 are essentially useless for dependency management. By giving a name and
 clear definition to the above ideas, it becomes easy to communicate your
 intentions to the users of your software. Once these intentions are clear,
 flexible (but not too flexible) dependency specifications’ can finally be made.
-```
 
 ## License
 
--- index.js
+++ index.js
@@ -7,12 +7,17 @@ import apostrophes from 'typographic-apostrophes';
 import quotes from 'typographic-quotes';
 import apostrophesForPlurals from 'typographic-apostrophes-for-possessive-plurals';
 
+import endashes from 'typographic-en-dashes';
+import emdashes from 'typographic-em-dashes';
+
 export const typographer = compose(
-  ellipses,
-  singleSpaces,
   apostrophes,
   quotes,
-  apostrophesForPlurals
+  apostrophesForPlurals,
+  endashes,
+  emdashes,
+  ellipses,
+  singleSpaces
 );
 
 /**
@@ -20,31 +25,31 @@ export const typographer = compose(
  * Btw, I modified them to reflect all used modules
  */
 export const sampleInput = `
-  In "the 'world' of software management" there exists a dread place called
-  "dependency hell."  The bigger your system grows and the more packages you
-  integrate into your software,  the more likely you are to find yourself,
-  one day, in this pit of despair...
+In "the 'world' of software management" there exists a dread place called
+"dependency hell."  The bigger your system grows and the more packages you
+integrate into your software —  the more likely you are to find yourself,
+one day, in this pit of despair...
 
-  'This is not a new or "revolutionary" idea'.  In fact,  you probably do something
-  close to this already.  The problem is that "close" isn't good enough.
-  Without compliance to some sort of formal specification,  version numbers
-  are essentially useless for dependency management.  By giving a name and
-  clear definition to the above ideas,  it becomes easy to communicate your
-  intentions to the users of your software.  Once these intentions are clear,
-  flexible (but not too flexible) dependency specifications' can finally be made.
+'This is not a new or "revolutionary" idea'.  In fact,  you probably do something
+close to this already in 1999-2014.  The problem -- that "close" isn't good enough.
+Without compliance to some sort of formal specification,  version numbers
+are essentially useless for dependency management.  By giving a name and
+clear definition to the above ideas,  it becomes easy to communicate your
+intentions to the users of your software.  Once these intentions are clear,
+flexible (but not too flexible) dependency specifications' can finally be made.
 `;
 
 export const sampleOutput = `
-  In “the ‘world’ of software management” there exists a dread place called
-  “dependency hell.” The bigger your system grows and the more packages you
-  integrate into your software, the more likely you are to find yourself,
-  one day, in this pit of despair…
+In “the ‘world’ of software management” there exists a dread place called
+“dependency hell.” The bigger your system grows and the more packages you
+integrate into your software — the more likely you are to find yourself,
+one day, in this pit of despair…
 
-  “This is not a new or ‘revolutionary’ idea”. In fact, you probably do something
-  close to this already. The problem is that “close” isn’t good enough.
-  Without compliance to some sort of formal specification, version numbers
-  are essentially useless for dependency management. By giving a name and
-  clear definition to the above ideas, it becomes easy to communicate your
-  intentions to the users of your software. Once these intentions are clear,
-  flexible (but not too flexible) dependency specifications’ can finally be made.
+“This is not a new or ‘revolutionary’ idea”. In fact, you probably do something
+close to this already in 1999–2014. The problem — that “close” isn’t good enough.
+Without compliance to some sort of formal specification, version numbers
+are essentially useless for dependency management. By giving a name and
+clear definition to the above ideas, it becomes easy to communicate your
+intentions to the users of your software. Once these intentions are clear,
+flexible (but not too flexible) dependency specifications’ can finally be made.
 `;
